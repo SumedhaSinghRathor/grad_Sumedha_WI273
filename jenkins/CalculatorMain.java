27229244c936cd907e647902aa4a7d186778fea9
SumedhaSinghRathor <sumedharathor@gmail.com>
2026-02-25 12:24:22 +0530

    remove jenkins demo

--- jenkins/CalculatorMain.java
+++
@@ -1,9 +0,0 @@
-public class CalculatorMain extends Calculator {
-    public static void main(String[] args) {
-        if (args[0] == "add") {
-            System.out.println(add(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
-        } else if (args[0] == "sub") {
-            System.out.println(sub(Integer.parseInt(args[1]), Integer.parseInt(args[2])));
-        }
-    }
-}
